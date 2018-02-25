a8b284c521ae370452e9feeaab237e43a2850324
nira <nayzaa2010@hotmail.com>
2018-02-25 15:02:01 +0700

    test2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
           <?php
-          require 'h.php';
+          require 'resources/views/h.php';
           ?>
             <div class="content">
                 <div class="title">You can call me<br><b>'Bobby'</b></div>
